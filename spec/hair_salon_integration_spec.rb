635a88140677e28feed72f552b9b85d5114413f1
Devin Ludwig <devinludwig@gmail.com>
2016-12-10 16:08:14 -0800

    complete integration testing with passing specs.

--- spec/hair_salon_integration_spec.rb
+++ spec/hair_salon_integration_spec.rb
@@ -56,12 +56,38 @@ describe("the display client page path", {:type => :feature}) do
 end
 
 describe("the delete client path", {:type => :feature}) do
-  it("displays a client's page with current stylist when a user clicks the client's name.") do
+  it("deletes a client from the database when user click the 'Delete This Client' button.") do
     visit('/')
     fill_in('new_client', :with => 'Yoko')
     click_button("Add Client")
     click_link('Yoko')
     click_button("Delete This Client")
     expect(page).to_not have_content("Yoko")
+  end
+end
+
+describe("the update client's stylist path", {:type => :feature}) do
+  it("changes a client's stylist when user selects a new stylist and clicks the update button.") do
+    visit('/')
+    fill_in('new_stylist', :with => 'Julia')
+    click_button("Add Stylist")
+    fill_in('new_client', :with => 'Yoko')
+    click_button("Add Client")
+    click_link('Yoko')
+    find('#stylist_update').find(:xpath, 'option[1]').select_option
+    click_button('Update Stylist')
+    click_link('Julia')
+    expect(page).to have_content("Julia's Clients: Yoko")
+  end
+end
+
+describe("the return to main page from client page path", {:type => :feature}) do
+  it("returns to the main page from a client's page when user clicks the link") do
+    visit('/')
+    fill_in('new_client', :with => 'Yoko')
+    click_button("Add Client")
+    click_link('Yoko')
+    click_link('Return to Main Page')
+    expect(page).to have_content("Welcome to Devin's Hair Salon")
   end
 end
